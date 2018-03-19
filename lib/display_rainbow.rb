ea2891614469a0841fee429ee6269b7e3d9bc24b
Liebe Brand <liebebrand@gmail.com>
2018-03-19 14:37:50 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts 
+end
+
+puts 
+colors  ["R": 'red', 
+        "O": 'orange', 
+        "Y": 'yellow', 
+        "G": 'green', 
+        "B": 'blue', 
+        "I": 'indigo', 
+        "V": 'violet']
+
+
